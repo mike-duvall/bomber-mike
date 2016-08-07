7e149a7ac18611eb2d65eee93f58ada9447bc136
Mike DuVall <mike.a.duvall@gmail.com>
2016-08-07 09:10:37 -0500

    Updated bitmaps path

--- gamestate/SelectingNumPlayers.cpp
+++ gamestate/SelectingNumPlayers.cpp
@@ -39,7 +39,7 @@ GameState * SelectingNumPlayers::GetNextGameStateAndDeleteCurrentGameState()
 
 SelectingNumPlayers::SelectingNumPlayers()
 {
-	string filename = "../bitmaps/Selecting/HowManyPlayers.bmp";
+	string filename = "Selecting/HowManyPlayers.bmp";
 	gameObject = new SimpleGameObject(70,55, 478,370,filename);
 
 
