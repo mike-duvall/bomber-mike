06d4627c46c7e5fe8ad2aac85b7c6636846cb76e
Mike DuVall <mike.a.duvall@gmail.com>
2016-08-26 07:19:29 -0500

    Next

--- gamestate/Playing.cpp
+++ gamestate/Playing.cpp
@@ -25,52 +25,13 @@ using namespace std;
 
 Playing::Playing()
 {
-	soundChannel = Sound::PlaySound(Sound::playing, .5f);
-// 	clock = new SimpleGameObject(30,10,30,40,"bitmaps/InGame/Scoreboard/Clock.bmp",_RGB32BIT(0,0,182,0));
-// 	timer = new Timer(70,25);
-	clock = new SimpleGameObject(10,-40,30,40,"bitmaps/InGame/Scoreboard/Clock.bmp",_RGB32BIT(0,0,182,0));
-	timer = new Timer(50,-25);
-
-	SetScoreIconLocations();
-
 
 }
 
-
-void Playing::SetScoreIconLocations()
-{
-	PLAYER_VECTOR::iterator theIterator;
-
-	int playerIndex = 0;
-
-
-	for(	theIterator = Universe::GetPlayers().begin();
-		theIterator != Universe::GetPlayers().end();
-		theIterator++
-		)
-	{
-		Player * next = *theIterator;
-
-		int playerType = next->GetPlayerType();
-		int x = 135 + (playerType * 65) - 20;
-		int y = 15 - 50;
-
-		next->SetScoreIconPos(x,y);
-//		next->GetScoreIcon()->GetBlitterObject()->Set_Pos(x,y);
-		next->GetScoreShell()->GetBlitterObject()->Set_Pos(x + 40,y + 5);
-		next->GetScoreNumber()->GetBlitterObject()->Set_Pos(x + 40, y + 5);
-
-	}
-
-}
 
 Playing::~Playing()
 {
-	soundChannel->stop();
 }
-
-
-
 
 
 
