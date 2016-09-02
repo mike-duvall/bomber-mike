72a2ffe431af71af2178de7967813863014f11e9
Mike DuVall <mike.a.duvall@gmail.com>
2016-09-02 07:09:36 -0500

    Next

--- main/main.cpp
+++ main/main.cpp
@@ -13,15 +13,19 @@
 #include <dinput.h>
 
 #include "../t3dlib/t3dlib1.h"
+#include "../t3dlib/ddutil.h"
 
-#include "../bob/BlitterObject.h"
 
-#include "../gameobjects/Player.h"
 
 #include "../input/Keyboard.h"
 #include "../universe/Point.h"
 
 #include "../gameobjects/ControlEvent.h"
+
+#include "../t3dlib/t3dlib1.h"
+
+#include "../bob/BlitterObject.h"
+
 
 using namespace std;
 
@@ -30,25 +34,83 @@ extern HINSTANCE main_instance;
 extern HWND      main_window_handle;
 
 
-
-
 LPDIRECTINPUT8        lpdi      = NULL;    // dinput object
-
 
 
 #define TOTAL_NUM_ROWS 11
 #define TOTAL_NUM_COLUMNS 13
 
 Keyboard * theKeyboard;
-Player * thePlayer;
+//Player * thePlayer;
+BlitterObject * theBlitterObject_;
 int mapLeftX;
 int mapTopY;
+
+int moveIncrementAmount;
+int initialX;
+int initialY;
+
+Controller * theController;
+
+
+
+
+void CreateBlitterObject(int x, int y)
+{
+	x -= 6;
+	y -= 12;
+
+	initialX = x;
+	initialY = y;
+
+	moveIncrementAmount = 2;
+
+	int playerWidth = 128;
+	int playerHeight = 128;
+
+	int totalNumFrames = 1;
+	int transparentColorKey = RGB(0, 107, 0);
+
+
+	theBlitterObject_ = new BlitterObject(x, y, playerWidth, playerHeight, totalNumFrames,
+		BOB_ATTR_VISIBLE | BOB_ATTR_MULTI_ANIM, DDSCAPS_VIDEOMEMORY, transparentColorKey);
+
+	int animations[] = { 0 };
+
+	string shortFileName = "WhitePlayerMushroom.bmp";
+
+
+	string pathPrefix = "";
+	string longFilename = "bitmaps/" + shortFileName;
+
+	// load in new bitmap file
+	IDirectDrawSurface7 * dds = DDLoadBitmap(lpdd, longFilename.c_str(), 0, 0);
+	if (dds == 0)
+	{
+		throw "Could not load bitmap";
+	}
+
+
+	theBlitterObject_->Load_Frame(dds, 0, 0, 0, BITMAP_EXTRACT_MODE_CELL);
+
+	Unload_Bitmap_File(&bitmap8bit);
+
+	theBlitterObject_->Load_Animation(0, 1, animations);
+
+
+	theBlitterObject_->Set_Animation(0);
+	theBlitterObject_->Set_Anim_Speed(8);
+	theBlitterObject_->Set_Vel(0, 0);
+
+	theBlitterObject_->Set_Pos(initialX, initialY);
+}
+
 
 
 int Game_Init(void *parms,  int num_parms)
 {
 
-	int windowed = false;
+	int windowed = true;
 	DDraw_Init(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, windowed);
 
 	// first create the direct input object
@@ -56,7 +118,6 @@ int Game_Init(void *parms,  int num_parms)
 		return(0);
 
 	theKeyboard = new Keyboard(lpdi, main_window_handle);
-
 
 	// set clipping rectangle to screen extents so mouse cursor
 	// doens't mess up at edges
@@ -66,8 +127,10 @@ int Game_Init(void *parms,  int num_parms)
 	// hide the mouse
 	ShowCursor(FALSE);
 
-	thePlayer = new Player(32, 32);
-	thePlayer->SetController(theKeyboard);
+//	thePlayer = new Player(32, 32);
+	CreateBlitterObject(32, 32);
+	//thePlayer->SetController(theKeyboard);
+	theController = theKeyboard;
 
 	//int columnOffset = 13 * 32;
 	//int rowOffset = 11 * 32;
@@ -79,7 +142,11 @@ int Game_Init(void *parms,  int num_parms)
 
 } // end Game_Init
 
-///////////////////////////////////////////////////////////
+
+
+
+
+
 
 int Game_Shutdown(void *parms,  int num_parms)
 {
@@ -92,6 +159,85 @@ int Game_Shutdown(void *parms,  int num_parms)
 ///////////////////////////////////////////////////////////
 
 
+void HandleMoveDown()
+{
+	theBlitterObject_->IncrementY(moveIncrementAmount);
+}
+
+
+
+void HandleMoveUp()
+{
+	theBlitterObject_->IncrementY(-moveIncrementAmount);
+}
+
+
+void HandleMoveRight(int controlEvent)
+{
+	theBlitterObject_->IncrementX(moveIncrementAmount);
+}
+
+
+void HandleMoveLeft(int controlEvent)
+{
+	theBlitterObject_->IncrementX(-moveIncrementAmount);
+}
+
+
+
+void Mushroom_Update(int controlEvent)
+{
+
+	switch (controlEvent)
+	{
+
+	case CONTROL_EVENT_MOVE_EAST:
+		HandleMoveRight(controlEvent);
+		break;
+
+
+	case CONTROL_EVENT_MOVE_WEST:
+		HandleMoveLeft(controlEvent);
+		break;
+
+	case CONTROL_EVENT_MOVE_SOUTH:
+		HandleMoveDown();
+		break;
+
+	case CONTROL_EVENT_MOVE_NORTH:
+		HandleMoveUp();
+		break;
+
+	}
+
+}
+
+
+
+
+bool Mushroom_Update()
+{
+	int controlEvent = CONTROL_EVENT_DO_NOTHING;
+
+	controlEvent = theController->GetControlEvent();
+	Mushroom_Update(controlEvent);
+
+	return false;
+}
+
+
+
+
+
+void Mushroom_Draw(LPDIRECTDRAWSURFACE7 dest)
+{
+	theBlitterObject_->Draw(dest);
+}
+
+
+
+
+
 void Update()
 {
 
@@ -99,8 +245,15 @@ void Update()
 	DWORD backgroundColor = _RGB32BIT(0, 0, 0, 0);
 	DDraw_Fill_Surface(lpddsback, backgroundColor);
 
-	thePlayer->Update();
-	thePlayer->Draw(lpddsback);
+
+	//Pull over remaining Player methods as non-object methods
+//		Then delete Player.cpp and H
+
+	//thePlayer->Update();
+	Mushroom_Update();
+
+	//thePlayer->Draw(lpddsback);
+	Mushroom_Draw(lpddsback);
 
 	DDraw_Flip();
 
